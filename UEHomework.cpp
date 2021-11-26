e64760a8df790cccf1d02e57394f6c0ea4f30ac5
Nikk <nikkigazejr@gmail.com>
2021-11-26 21:21:07 +0300

    Print even/odd

--- UEHomework.cpp
+++ UEHomework.cpp
@@ -2,8 +2,24 @@
 #include "Helpers.h"
 
 using namespace std;
+
+const int N = 10;
+
+static void printNumbers(bool printOdd, int count)
+{
+	for (int i = 1; i <= count; i++)
+	{
+		if (i % 2 == (int)printOdd)
+		{
+			cout << i << " ";
+		}
+	}
+}
+
+
 int main()
 {
+
 	int a = 4;
 	int b = 5;
 	int c = SquaredSum(a, b);
@@ -15,4 +31,14 @@ int main()
 	cout << s[0] << endl;
 	cout << s[s.length() - 1] << endl;
 
+	for (int i = 1; i <= N; i++)
+	{
+		if (i % 2 == 0)
+		{
+			cout << i << " ";
+		}
+	}
+
+	cout << endl;
+	printNumbers(false, N);
 }
