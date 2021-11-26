6988bac790627d1004c9a4ff89fc537440e2a42b
Nikk <nikkigazejr@gmail.com>
2021-11-26 21:11:20 +0300

    Initial

---
+++ Helpers.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int SquaredSum(int a, int b)
+{
+	return (a + b) * (a + b);
+}
---
+++ UEHomework.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+using namespace std;
+int main()
+{
+	int a = 4;
+	int b = 5;
+	int c = SquaredSum(a, b);
+	cout << c << endl;
+}
